b00356ec2cc70869a06b5265422628ad69563208
Jason Gerard <jasongerard321@gmail.com>
2023-05-17 20:49:07 -0400

    Add base file for yen's algo, impl TODO

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,1 +1,2 @@
 pub mod dijkstra;
+pub mod yen;
---
+++ src/algorithms/yen.rs
@@ -0,0 +1,11 @@
+use crate::contact::{Contact, Node};
+use crate::route::Route;
+
+pub fn cgr_yen(
+    root_contact: &mut Contact,
+    destination: Node,
+    mut contact_plan: Vec<Contact>,
+    top_k: u32,
+) -> Option<Route> {
+    todo!()
+}
